953bd2736077b85fbf31b9e1ff371cd9121aa6c2
Aleksandar Sokolovic <78912243+School199@users.noreply.github.com>
2021-12-07 16:00:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-alex:Alex20031992@cluster0.57iax.mongodb.net/todolistDB",{useNewUrlParser:true});
 
 const itemsSchema = {
     name: String
